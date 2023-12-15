26d37d834f52cbbbbfe9ba1410f89bd79e10059a
sinano1107 <tftf161107@icloud.com>
2023-12-15 20:23:49 +0900

    Update register page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,29 +1,60 @@
 'use client'
 
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  AuthErrorCodes,
+} from 'firebase/auth'
+import { useState } from 'react'
 
 import '../../lib/FirebaseConfig'
 
 export default function Auth() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
   const doRegister = async () => {
     const auth = getAuth()
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        'aaa@aaa.aaa',
-        'password',
-      )
+      await createUserWithEmailAndPassword(auth, email, password)
       alert('登録完了！')
-    } catch (e) {
-      console.error(error)
+    } catch (error) {
+      // ? エラーコードはここを参照：https://firebase.google.com/docs/reference/js/auth?hl=ja#autherrorcodes
+      switch (error.code) {
+        case AuthErrorCodes.INVALID_EMAIL:
+          setError('メールアドレスが無効です')
+          break
+        case AuthErrorCodes.WEAK_PASSWORD:
+          setError('パスワードが弱いです')
+          break
+        default:
+          setError(`エラーが発生しました\nエラーコード:${error.code}`)
+      }
     }
   }
 
   return (
     <main style={{ minHeight: '100vh' }}>
       <h1>register</h1>
-      <button onClick={doRegister}>登録</button>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+      />
+      <button
+        onClick={doRegister}
+        disabled={email.length === 0 || password.length === 0}
+      >
+        登録
+      </button>
+      <p style={{ color: 'red' }}>{error}</p>
     </main>
   )
 }
