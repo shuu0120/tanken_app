bbb82ac8f4eb53606b76db5c1840d4158c41f870
sinano1107 <tftf161107@icloud.com>
2023-12-15 21:31:25 +0900

    Update サインイン後にホームページへリダイレクト

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,16 +1,20 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 import '../../lib/FirebaseConfig'
 
 export default function Signin() {
+  const router = useRouter()
+
   const doSignin = async () => {
     const auth = getAuth()
 
     try {
       await signInWithEmailAndPassword(auth, 'aa@aaa.aaa', 'password')
       alert('サインイン成功')
+      router.replace('/')
     } catch (e) {
       console.error(e)
     }
